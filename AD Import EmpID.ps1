e99518aad4e9b6e2b92fd6bc72e486613b7197a4
Jeremy Armstrong <101134271+jarmstrong155@users.noreply.github.com>
2023-07-16 18:40:49 -0700

    Add files via upload

---
+++ AD Import EmpID.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module ActiveDirectory
+$Users = Import-Csv -Path c:\users\jeremy.armstrong\documents\imports\empidimport2.csv
+Get-Content -Path c:\users\jeremy.armstrong\documents\imports\empidimport2.csv | ft
+sleep 10
+foreach ($User in $Users)
+{
+Set-ADUser -identity $User.SamAccountName -replace @{"EmployeeID" = $User.EmployeeID} -verbose
+}
